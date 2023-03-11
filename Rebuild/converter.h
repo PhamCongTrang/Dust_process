3e0a08dfa245e039e71d68bba5b461523053b016
unknown <trang.pc200631@sis.hust.edu.vn>
2023-03-11 09:41:47 +0700

    First 11/03

--- Rebuild/converter.h
+++ Rebuild/converter.h
@@ -14,7 +14,7 @@ struct dust_t
     char timestamp[20];
     float value;
     int aqi;
-    char pollution[50];
+    char pollution[80];
 
     char start_byte[6];
     char packet_length[6];
@@ -33,7 +33,6 @@ int import_csv_to_dust_t(char* f_name_csv, struct dust_t * dust)
 {
     FILE* f;
     f = fopen(f_name_csv, "r");
-    //printf("fopen: %d\n",f);
     struct dust_t temp;
     char line[50];
     fgets(line, 50,f); // Bo qua dong dau
@@ -77,10 +76,55 @@ int import_hex_to_dust_t(char* f_name_hex, struct dust_t * dust)
     int i = 0;
     while (fgets(line,sizeof(line),f))
     {
-        char * pch;
-
+        int k = 0;
+        for(int i = 0; i < 2; i++)
+        {
+            temp.start_byte[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 2; i++)
+        {
+            temp.packet_length[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 2; i++)
+        {
+            temp.sensor_id_hex[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 11; i++)
+        {
+            temp.timestamp_hex[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 11; i++)
+        {
+            temp.value_hex[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 5; i++)
+        {
+            temp.aqi_hex[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 2; i++)
+        {
+            temp.checksum[i] = line[k];
+            k++;
+        }
+        k++;
+        for(int i = 0; i < 2; i++)
+        {
+            temp.stop_byte[i] = line[k];
+            k++;
+        }
         dust[i] = temp;
-        pch = strtok(NULL,"\n");
         i++;
     }
     fclose(f);
@@ -107,48 +151,6 @@ int sum_byte(char *c)
     }
     return sum;
 }
-/*
-* Use to convert csv info to hex. Print to f_name_hex
-* dust_t before: {sensor_id, timestamp, value, aqi, pollution}
-* dust_t after: add {start_byte, packet_length, sensor_id_hex, timestamp_hex, value_hex, aqi_hex, checksum, stop_byte}
-*/
-void convert_csv_to_hex(struct dust_t * dust, int size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        //-- start_byte_hex
-        strcpy(dust[i].start_byte, "00");
-        //--packet_length_hex
-        strcpy(dust[i].packet_length, "0F");
-        //--sensor_id_hex
-        itoa(dust[i].sensor_id,dust[i].sensor_id_hex,16);
-        //--timestamp_hex
-        struct tm local;
-        local.tm_year = ((dust[i].timestamp)[0]-48)*1000 + ((dust[i].timestamp)[1]-48)*100 + ((dust[i].timestamp)[2]-48)*10 + ((dust[i].timestamp)[3]-48)*1 - 1900;
-        local.tm_mon = ((dust[i].timestamp)[5]-48)*10 + ((dust[i].timestamp)[6]-48)*1 - 1;
-        local.tm_mday = ((dust[i].timestamp)[8]-48)*10 + ((dust[i].timestamp)[9]-48)*1;
-        local.tm_hour = ((dust[i].timestamp)[11]-48)*10 + ((dust[i].timestamp)[12]-48)*1 ;
-        local.tm_min = ((dust[i].timestamp)[14]-48)*10 + ((dust[i].timestamp)[15]-48)*1;
-        local.tm_sec = ((dust[i].timestamp)[17]-48)*10 + ((dust[i].timestamp)[18]-48)*1;
-        time_t tim = mktime(&local) + 7*3600;
-        itoa(tim,dust[i].timestamp_hex,16);
-        //--value_hex
-        unsigned int temp;
-        temp = *(unsigned int*) &dust[i].value;
-        itoa(temp, dust[i].value_hex, 16);
-        //--aqi_hex
-        itoa(dust[i].aqi,dust[i].aqi_hex,16);
-        //--checksum
-        int sum = 15 + sum_byte(dust[i].sensor_id_hex) + sum_byte(dust[i].timestamp_hex) 
-                    + (dust[i].aqi_hex) + sum_byte(dust[i].value_hex);
-        uint8_t sum_last_2 = sum % 256;
-        sum_last_2 = ~ sum_last_2 + 1;
-        itoa(sum_last_2, dust[i].checksum, 16);
-        //--stop_byte
-        strcpy(dust[i].stop_byte,"FF");
-    }
-     
-}
 void format_hex(char* c)
 {
     int n = strlen(c);
@@ -172,16 +174,66 @@ void format_hex(char* c)
         j++;
     }
 }
-void format_dust_hex(struct dust_t* dust, int size)
+void format_dust_hex(struct dust_t* dust, int i)
 {
-    for(int i = 0; i < size; i++ )
-    {
         format_hex(dust[i].sensor_id_hex);
         format_hex(dust[i].timestamp_hex);
         format_hex(dust[i].value_hex);
         format_hex(dust[i].aqi_hex);
+        /*
+        * Bo sung 00 cho du 2 byte
+        */
+        if(strlen(dust[i].aqi_hex) < 4) 
+        {
+            char temp_c[10] = "00 ";
+            strcat(temp_c,dust[i].aqi_hex);
+            strcpy(dust[i].aqi_hex,temp_c);
+        }
+        //format_hex(dust[i].checksum);
+}
+/*
+* Use to convert csv info to hex. Print to f_name_hex
+* dust_t before: {sensor_id, timestamp, value, aqi, pollution}
+* dust_t after: add {start_byte, packet_length, sensor_id_hex, timestamp_hex, value_hex, aqi_hex, checksum, stop_byte}
+*/
+void convert_csv_to_hex(struct dust_t * dust, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        //-- start_byte_hex
+        strcpy(dust[i].start_byte, "00");
+        //--packet_length_hex
+        strcpy(dust[i].packet_length, "0F");
+        //--sensor_id_hex
+        itoa(dust[i].sensor_id,dust[i].sensor_id_hex,16);
+        //--timestamp_hex
+        struct tm local;
+        local.tm_year = ((dust[i].timestamp)[0]-48)*1000 + ((dust[i].timestamp)[1]-48)*100 + ((dust[i].timestamp)[2]-48)*10 + ((dust[i].timestamp)[3]-48)*1 - 1900;
+        local.tm_mon = ((dust[i].timestamp)[5]-48)*10 + ((dust[i].timestamp)[6]-48)*1 - 1;
+        local.tm_mday = ((dust[i].timestamp)[8]-48)*10 + ((dust[i].timestamp)[9]-48)*1;
+        local.tm_hour = ((dust[i].timestamp)[11]-48)*10 + ((dust[i].timestamp)[12]-48)*1 ;
+        local.tm_min = ((dust[i].timestamp)[14]-48)*10 + ((dust[i].timestamp)[15]-48)*1;
+        local.tm_sec = ((dust[i].timestamp)[17]-48)*10 + ((dust[i].timestamp)[18]-48)*1;
+        time_t tim = mktime(&local) ; //+ 7*3600
+        itoa(tim,dust[i].timestamp_hex,16);
+        //--value_hex
+        unsigned int temp;
+        temp = *(unsigned int*) &dust[i].value;
+        itoa(temp, dust[i].value_hex, 16);
+        //--aqi_hex
+        itoa(dust[i].aqi,dust[i].aqi_hex,16);
+        //--checksum
+        format_dust_hex(dust,i);
+        int sum = 15 + sum_byte(dust[i].sensor_id_hex) + sum_byte(dust[i].timestamp_hex) 
+                    + sum_byte(dust[i].aqi_hex) + sum_byte(dust[i].value_hex);
+        uint8_t sum_last_2 = sum % 256;
+        sum_last_2 = ~ sum_last_2 + 1;
+        itoa(sum_last_2, dust[i].checksum, 16);
         format_hex(dust[i].checksum);
+        //--stop_byte
+        strcpy(dust[i].stop_byte,"FF");
     }
+     
 }
 void print_hex(struct dust_t* dust, int size, char * f_name_hex)
 {
@@ -202,6 +254,7 @@ int hex_to_dec(char *c)
 {
     int n = strlen(c);
     int sum = 0;
+    int k = (n+1)*2/3 - 1;
     for(int i = 0; i < n; i++ )
     {
         if(c[i] == ' ') continue;
@@ -209,12 +262,55 @@ int hex_to_dec(char *c)
         if(c[i] >= '0' && c[i] <= '9') dec = c[i] - '0';
         if(c[i] >= 'A' && c[i] <= 'F') dec = c[i] - 'A' + 10;
         if(c[i] >= 'a' && c[i] <= 'f') dec = c[i] - 'a' + 10;
-        sum += dec*round(pow(16,(n+1)*2/3 - i - 1)); 
+        sum += dec*round(pow(16,k)); 
+        k--;
     }
     return sum;
 }
-void convert_hex_to_csv(struct dust_t * dust, int size, char* f_name_csv)
+float hex_to_float(char* c)
 {
+    int temp = hex_to_dec(c);
+    return *(float *) &temp;
+}
+void pollution_rate(int aqi, char* st)
+{
+    if(aqi >= 0 && aqi < 50) 
+        strcpy(st,"Good");
+    if(aqi >= 50 && aqi < 100) 
+        strcpy(st,"Moderate");
+    if(aqi >= 100 && aqi < 150) 
+        strcpy(st,"Slightly unhealthy");
+    if(aqi >= 150 && aqi < 200) 
+        strcpy(st,"Unhealthy");
+    if(aqi >= 200 && aqi < 300) 
+        strcpy(st,"Very unhealthy");
+    if(aqi >= 300 && aqi < 400) 
+        strcpy(st,"Hazardous");
+    if(aqi >= 400 && aqi < 500) 
+        strcpy(st,"Extremely hazardous");
+}
+void convert_hex_to_csv(struct dust_t * dust, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        dust[i].sensor_id = hex_to_dec(dust[i].sensor_id_hex);
+        time_t tim = hex_to_dec(dust[i].timestamp_hex);
+        struct tm* local;
+        local = localtime(&tim);
+        strftime(dust[i].timestamp,29,"%Y:%m:%d %H:%M:%S",local);
+        dust[i].value = hex_to_float(dust[i].value_hex);
+        dust[i].aqi = hex_to_dec(dust[i].aqi_hex);
+        pollution_rate(dust[i].aqi,dust[i].pollution);
 
+    }
+}
+void print_csv(struct dust_t* dust, int size, char * f_name_csv)
+{
+    FILE* fcsv;
+    fcsv = fopen(f_name_csv,"w");
+    fprintf(fcsv,"ID,TIME,VALUE,AQI,POLLUTION");
+    for(int i = 0; i < size; i++)
+        fprintf(fcsv,"\n%d,%s,%.1f,%d,%s",dust[i].sensor_id,dust[i].timestamp,dust[i].value,dust[i].aqi,dust[i].pollution);
+    fclose(fcsv); 
 }
 #endif
--- Rebuild/dust_convert.c
+++ Rebuild/dust_convert.c
@@ -5,32 +5,47 @@
 #include <time.h>
 #include "converter.h"
 
-int main()
+int main(int argc, char* argv[])
 {
     /*
     * Vung khai bao bien toan cuc
     */
     struct dust_t dust[10000];
     int size;
-    char f_name_csv[30] = "dust_aqi.csv";
-    char f_name_hex[30] = "hex_dust.dat";
     /*
     * Convert CSV to Hex: Mode = 1
     * Convert Hex to CSV: Mode = -1
     */
-    int Mode = 1;
+    int Mode = 10;
+    // NEU MAY NHIN THAY DONG NAY THI XU LY NOT CHO THAM SO DONG LENH VA KIEM TRA DAU VAO DI
+    char source_file[20], target_file[20];
+    strcpy(source_file,argv[1]);
+    strcpy(target_file,argv[2]);
+    char source_extension[10], target_extension[10];
+    int x = argc;
+    for(int i = 1; i <= 4; i++)
+       source_extension[i] = source_file[strlen(source_file) - i]; 
+    for(int i = 1; i <= 4; i++)
+       target_extension[i] = target_file[strlen(target_file) - i];
+
+    printf("\n%s %s",source_extension,target_extension);
+    if(strcmp(source_extension,".csv") && strcmp(target_extension, ".dat")) Mode = 1;
+    if(strcmp(source_extension,".dat") && strcmp(target_extension, ".csv")) Mode = 0;
+    printf("%d",Mode);
+    
     if(Mode == 1 )
     {
-        size = import_csv_to_dust_t(f_name_csv, dust);
+        size = import_csv_to_dust_t(argv[1], dust);
         convert_csv_to_hex(dust,size);
         printf("%d",size);
-        format_dust_hex(dust,size);
-        print_hex(dust,size,f_name_hex);
+        //format_dust_hex(dust,size);
+        print_hex(dust,size, argv[2]);
     }
-    else
+    if(Mode == 0)
     {
-        size = import_hex_to_dust_t(f_name_hex, dust);
-        convert_hex_to_csv(dust,size, f_name_csv);
+        size = import_hex_to_dust_t(argv[1], dust);
+        convert_hex_to_csv(dust,size);
+        print_csv(dust,size, argv[2]);
     }
     return 0;
 }
